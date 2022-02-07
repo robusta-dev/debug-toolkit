1d33f116244dc793c5c21035f7c8610640beb2e9
Avi-Robusta <97387909+Avi-Robusta@users.noreply.github.com>
2022-02-07 18:36:23 +0200

    Added remote stack trace (#1)
    * remote stacktrace from 3.3+
    
    * by default print all thread stack-trace
    
    * version bump
    
    * reformatted stack trace to add more thread information
    
    added thread pid and name to stack trade
    
    * remove unnessairy line

---
+++ src/debug_toolkit/payloads/stack_trace.py
@@ -0,0 +1,26 @@
+def entrypoint(output_path: str):
+    import threading
+    import sys
+
+    ALL_THREADS = bool(ALL_THREADS_PLACEHOLDER)
+    stack_trace = get_traceback(ALL_THREADS)
+    for thr in threading.enumerate():
+        if sys.version_info[0] >= 3 and sys.version_info[1] >= 8:
+            #  _native_id is only from python3.8+
+            replacement_str = f"{thr.getName()} tid:{thr._native_id}"
+        else:
+            replacement_str = f"{thr.getName()} thread identity:{thr._ident}"
+        thread_id_str = '0x{:016x}'.format(thr._ident)
+        stack_trace = stack_trace.replace(thread_id_str, replacement_str)
+    with open(output_path, "w+") as output_file:
+        output_file.write(stack_trace)
+
+
+def get_traceback(all_threads: bool):
+    import tempfile
+    import faulthandler
+    with tempfile.TemporaryFile() as tmp:
+        faulthandler.dump_traceback(file=tmp, all_threads=all_threads)
+        tmp.seek(0)
+        return tmp.read().decode("utf-8")
+
