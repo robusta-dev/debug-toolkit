a8ee84482cc8f28bb1aa4ae21f5b020f9086f551
avi robusta <avi@robusta.dev>
2022-07-31 10:20:57 +0300

    added multiple stack trace support

--- src/debug_toolkit/payloads/stack_trace.py
+++ src/debug_toolkit/payloads/stack_trace.py
@@ -1,9 +1,25 @@
 def entrypoint(output_path: str):
     import threading
     import sys
+    import time
+    import json
 
     ALL_THREADS = bool(ALL_THREADS_PLACEHOLDER)
-    stack_trace = get_traceback(ALL_THREADS)
+    AMOUNT = int(AMOUNT)
+    SLEEP_DURATION_S = int(SLEEP_DURATION_S)
+
+    thread_stack_dumps=[]
+    for _ in range(AMOUNT):
+        stack_trace = get_traceback(ALL_THREADS)
+        formated_trace = format_stack_trace(stack_trace)
+        thread_stack_dumps.append({"time": time.time(), "trace": formated_trace})
+        time.sleep(SLEEP_DURATION_S)
+    with open(output_path, "w+") as output_file:
+        json_formatted_str = json.dumps(thread_stack_dumps, indent=4)
+        output_file.write(json_formatted_str)
+
+
+def format_stack_trace(stack_trace: str):
     for thr in threading.enumerate():
         if sys.version_info[0] >= 3 and sys.version_info[1] >= 8:
             #  _native_id is only from python3.8+
@@ -12,8 +28,7 @@ def entrypoint(output_path: str):
             replacement_str = f"{thr.getName()} thread identity:{thr._ident}"
         thread_id_str = '0x{:016x}'.format(thr._ident)
         stack_trace = stack_trace.replace(thread_id_str, replacement_str)
-    with open(output_path, "w+") as output_file:
-        output_file.write(stack_trace)
+    return stack_trace
 
 
 def get_traceback(all_threads: bool):
